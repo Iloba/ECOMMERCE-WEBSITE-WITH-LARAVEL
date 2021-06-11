40f40099428121703e7f0d93dd8853cf48a8be0f
Iloba <scarlettsney@gmail.com>
2021-06-11 16:17:47 +0100

    login-ui

--- resources/views/my_auth/login.blade.php
+++ resources/views/my_auth/login.blade.php
@@ -1,29 +1,39 @@
 @extends('layouts.app')
 @section('content')
-  <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-4">
-                &nbsp;
-            </div>
-            <div class="col-md-4">
-                <h2>Welcome back,</h2>
-                <h5>Kindly fill in details to log in</h5>
-                <img src="{{asset('img/relax.svg')}}" class="img-fluid" alt="">
-            </div>
-            <div class="col-md-4">
-                <form action="">
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <span class="input-group-text" id="basic-addon1">@</span>
+  <div class="container mt-5 login-bg">
+        <div class="row justify-content-center">
+          
+                <div class="col-md-8">
+                    <div class="d-flex">
+                        <div class="img-box m-5">
+                            <h1 class="bolder">Welcome back,</h1>
+                            <h4 class="bold">Kindly fill in details to log in</h4>
+                            <img src="{{asset('img/relax.svg')}}" class="img-fluid relax" alt="">
                         </div>
-                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-                      </div>
-                </form>
-               </div>
-            </div>
-            <div class="col-md-4">
-                &nbsp;
-            </div>
+                        <div class="form-box mt-5 p-3">
+                            <form action="">
+                                 <div class="mb-3">
+                                    <input type="text" class="form-control" placeholder="Email Address" aria-label="Username" aria-describedby="basic-addon1">
+                                </div>
+                            
+                                <div class="mb-3">
+                                    <input type="password" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1">
+                                </div>
+
+                                <div class="float-right clearfix">
+                                    <p class="forgot-pass">forgot password?</p>
+                                </div>
+
+                                <div class="btn-box mt-5">
+                                    <button class="sign-in" type="submit">Sign In</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            
         </div>
   </div> 
+  @include('layouts.footer')
 @endsection
